06e430cf63d713a9ed6e6f1b4ac3aa361baf5904
Matteo Barbieri <neopard@gmail.com>
2025-09-29 21:19:34 +0200

    Implement card art change

---
+++ scripts/card_database.gd
@@ -0,0 +1,5 @@
+const CARDS = { # Attack, Health, 
+	"Warrior" : [2, 3],
+	"Wood_Elf" : [1, 1],
+	"Wizard" : [5, 7],
+}
--- scripts/deck.gd
+++ scripts/deck.gd
@@ -1,22 +1,22 @@
 extends Node2D
 
-var player_deck = ["Knight", "Knight", "Knight"]
+var player_deck = ["Warrior", "Wood_Elf", "Wizard", "Warrior"]
 
 const CARD_SCENE_PATH = "res://scenes/card.tscn"
-const CARD_DRAW_SPEED = 1
+const CARD_DRAW_SPEED = 0.2
 
 var player_hand_reference
+var card_database_reference
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
 	player_hand_reference = $"../PlayerHand"
 	$RichTextLabel.text = str(player_deck.size())
 	
-
-
-func draw_card():
+	card_database_reference = preload("res://scripts/card_database.gd")
 	
+func draw_card():
+	player_deck.shuffle()
 	var card_drawn = player_deck[0]
 	player_deck.erase(card_drawn)
 	
@@ -28,8 +28,15 @@ func draw_card():
 		$RichTextLabel.visible = false
 	
 	var card_scene = preload(CARD_SCENE_PATH) 
-	
 	var new_card = card_scene.instantiate()
+	var card_image_path = str("res://assets/ai_" + card_drawn.to_lower() + "_card.png")
+	
+	
+	new_card.get_node("CardImage").texture = load(card_image_path)
+	new_card.get_node("Attack").text = str(card_database_reference.CARDS[card_drawn][0])
+	new_card.get_node("Health").text = str(card_database_reference.CARDS[card_drawn][1])
+	
+	
 	%CardManager.add_child(new_card)
 	new_card.name = "Card"
 	player_hand_reference.add_card_to_hand(new_card, CARD_DRAW_SPEED)
